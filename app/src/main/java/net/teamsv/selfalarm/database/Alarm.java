e4573d8803017651a29782ac924fa34225cdd575
nolsigan <hiayuna@snu.ac.kr>
2016-04-28 21:07:34 +0900

    Change alarm schema date to h, m string

--- app/src/main/java/net/teamsv/selfalarm/database/Alarm.java
+++ app/src/main/java/net/teamsv/selfalarm/database/Alarm.java
@@ -1,22 +1,34 @@
 package net.teamsv.selfalarm.database;
 
-import java.util.Date;
-
 import io.realm.RealmObject;
 
 public class Alarm extends RealmObject {
 
-    private Date date;
+    private int hour;
+    private int minute;
     private Record record;
     private boolean onOff;
 
     /* set funcs */
-    public void setDate(Date in) { date = in; }
+    public void setTime(int h, int m) { hour = h; minute = m; }
     public void setRecord(Record in) { record = in; }
     public void setOnoff(boolean in) { onOff = in; }
 
     /* get funcs */
-    public Date getDate() { return date; }
+    public int getHour() { return hour; }
+    public int getMinute() { return minute; }
+    public String getTime() {
+
+        String ret = "";
+
+        if(hour < 10) ret += " ";
+        ret += String.valueOf(hour) + " : ";
+
+        if(minute < 10) ret += " ";
+        ret += String.valueOf(minute);
+
+        return ret;
+    }
     public Record getRecord() { return record; }
     public boolean getOnoff() { return onOff; }
 }
